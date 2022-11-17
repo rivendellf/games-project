1a906b7537aa100f3ea3a7b304ffc84e2e31ff84
Aisha Hamid <Aish@jonathons-mbp.home>
2022-11-17 12:20:09 +0000

    removed utils file

--- models/postCommentModel.js
+++ models/postCommentModel.js
@@ -2,7 +2,7 @@ const db = require("../db/connection");
 const { checkReviewIdExists } = require("../utils/utils.db");
 
 exports.insertCommentByReviewId = (body, review_id) => {
-  // return checkReviewIdExists(review_id).then(() => {
+
   const { username, comment } = body;
   const queryValues = [username, comment, review_id];
   const queryStr = `INSERT INTO comments (author, body, review_id)
@@ -14,4 +14,3 @@ VALUES ($1, $2, $3) RETURNING *;`;
     return res.rows[0];
   });
 };
-// };
